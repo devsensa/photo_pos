0ebce238c0a625350b4692b0d9c245bcc2332890
sensa <person.sensa@gmail.com>
2019-10-13 16:54:54 +0500

    WIP Alpha version

--- lib/app/domain/products/model.dart
+++ lib/app/domain/products/model.dart
@@ -1,5 +1,11 @@
 class Products {
   Products.empty();
+  final int count = 0;
+  final List<Product> products = [];
 }
 
-class Product {}
+class Product {
+  final String title;
+  final String price;
+  Product(this.title, this.price);
+}
