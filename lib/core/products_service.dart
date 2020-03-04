2afbd638cc81fc13bba9f3d36000d303c5080965
Devsensa <person.sensa@gmail.com>
2020-03-05 01:38:29 +0300

    add method stubs to product service

--- lib/core/products_service.dart
+++ lib/core/products_service.dart
@@ -1,1 +1,14 @@
-abstract class ProductsService {}
+abstract class ProductsService {
+
+  Future<void> addProduct(AddProduct action) async {}
+  
+  Future<void> removeProduct(RemoveProduct action) async {}
+  
+  Future<void> changeProductFavoriteMark(ChangeProductFavoriteMark action) async {}
+  
+  Future<void> changeProductTitle(ChangeProductTitle action) async {}
+  
+  Future<void> changeProductAmount(ChangeProductAmount action) async {}
+  
+  Future<void> changeProductPrice(ChangeProductPrice action) async {}
+}
